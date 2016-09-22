51219627bc1062358b83352a3519c90b290f391a
Pedro Guerra <pedromguerra@gmail.com>
2016-09-22 16:15:44 -0400

    cambios

--- test.js
+++ test.js
@@ -1,3 +1,16 @@
+var promise = new Promise(function(resolve, reject) {
+  // do a thing, possibly async, then…
+  var swith = true;
+
+  if (true /* everything turned out fine */) {
+    resolve("Stuff worked!");
+  }
+  else {
+    reject(Error("It broke"));
+  }
+});
+
+
 promise.then(function(result) {
   console.log(result); // "Stuff worked!"
 }, function(err) {
